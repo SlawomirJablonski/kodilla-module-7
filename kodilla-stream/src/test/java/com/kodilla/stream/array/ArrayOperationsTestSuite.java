1cf3246602a002a38cc647a66975c67b649510a8
SlawomirJablonski <forest19@interia.pl>
2018-10-26 14:33:31 +0200

    added ex7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,26 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage(){
+        //Given
+
+        int[] array = new int[20];
+        for (int i=0;i<array.length;i++){
+            array[i]=50+i;
+        }
+
+        //When
+        double result = ArrayOperations.getAverage(array);
+
+        //Then
+        int sum = Arrays.stream(array).sum();
+        double expectedAverage = (double)sum/array.length;
+        Assert.assertEquals(expectedAverage,result,0);
+    }
+}
