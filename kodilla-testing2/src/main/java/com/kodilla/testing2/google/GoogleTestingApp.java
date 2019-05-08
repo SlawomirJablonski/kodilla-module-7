427d8a1071dc659d0efcdaa6929da0c0b01fc18b
SlawomirJablonski <forest19@interia.pl>
2019-05-08 18:07:55 +0200

    mod31.2 dopasowane do terownika chrome74.0.3729.131

--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -6,14 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class GoogleTestingApp {
-    public static final String SEARCHFIELD = "fakebox-text";
+    public static final String SEARCHFIELD = "q";
 
     public static void main(String[] args){
 
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.google.com");
 
-        WebElement searchField = driver.findElement(By.id(SEARCHFIELD));
+        WebElement searchField = driver.findElement(By.name(SEARCHFIELD));
         searchField.sendKeys("Kodilla");
         searchField.submit();
     }
